58d43bb522efa52357736d374ebf7cbce2d828fe
sciencesakura <sakurasakurascience@gmail.com>
2020-03-30 06:33:10 +0900

    Renewal

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-import {dom, library} from '@fortawesome/fontawesome-svg-core';
-import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
-import {faLink} from '@fortawesome/free-solid-svg-icons';
-import {faGithub, faTwitter} from '@fortawesome/free-brands-svg-icons';
-
-library.add(faEnvelope, faLink, faGithub, faTwitter);
-dom.i2svg();
