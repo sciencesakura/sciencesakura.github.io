aa5a94204aa8d4c11b4204dca8788b71f3192515
sciencesakura <sakurasakurascience@gmail.com>
2020-04-20 14:43:12 +0900

    Reduce icon font to load

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import {dom, library} from '@fortawesome/fontawesome-svg-core';
+import {faFacebookSquare, faGithub, faTwitter} from '@fortawesome/free-brands-svg-icons';
+import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
+import {faLink, faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faFacebookSquare, faGithub, faTwitter, faEnvelope, faLink, faMapMarkerAlt);
+dom.i2svg();
