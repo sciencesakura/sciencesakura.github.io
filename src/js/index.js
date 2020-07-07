5a3f55cfea9be91a3385104c6adbce47f5bc8ac0
sciencesakura <sakurasakurascience@gmail.com>
2020-07-07 12:06:54 +0900

    Renewal

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,18 @@
-import {dom, library} from '@fortawesome/fontawesome-svg-core';
-import {faFacebookSquare, faGithub, faTwitter} from '@fortawesome/free-brands-svg-icons';
-import {faEnvelope} from '@fortawesome/free-regular-svg-icons';
-import {faLink, faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons';
+import { dom, library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faFacebookSquare,
+  faGithub,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faLink, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faFacebookSquare, faGithub, faTwitter, faEnvelope, faLink, faMapMarkerAlt);
+library.add(
+  faFacebookSquare,
+  faGithub,
+  faTwitter,
+  faEnvelope,
+  faLink,
+  faMapMarkerAlt
+);
 dom.i2svg();
